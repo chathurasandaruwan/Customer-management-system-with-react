ad0f1239f4203ffdf62fe96966a37d42c400856b
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-12-12 14:25:59 +0530

    create table component

---
+++ src/componentes/Table.tsx
@@ -0,0 +1,29 @@
+import DataTable from 'react-data-table-component';
+import {useContext} from "react";
+import {CustomerContext} from "./CustomerProvider.tsx";
+import {Customer} from "../models/Customer.ts";
+
+export function Table() {
+    const [customers,setCustomer] =useContext<Customer[]>(CustomerContext)
+    const columns = [
+        {
+            name: "Name",
+            selector: (row:Customer) => row.name,
+        },
+        {
+            name: "Email",
+            selector: (row:Customer) => row.email,
+        },
+        {
+            name: "Phone",
+            selector: (row:Customer) => row.phone,
+        }
+    ];
+    return (
+        <>
+            <div className="container my-5">
+                <DataTable columns={columns} data={customers} />
+            </div>
+        </>
+    );
+}
