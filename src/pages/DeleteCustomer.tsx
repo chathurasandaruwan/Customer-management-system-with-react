3de21e42717851378ecffbd507a3b805b3d7908f
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-12-12 13:21:25 +0530

    create delete customer page temp

--- src/pages/DeleteCustomer.tsx
+++ src/pages/DeleteCustomer.tsx
@@ -1,5 +1,31 @@
+import {useContext, useState} from "react";
+import {CustomerContext} from "../componentes/CustomerProvider.tsx";
+
 export function DeleteCustomer (){
+    const [customers , setCustomer] = useContext(CustomerContext)
+
+    // const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    // const [phone, setPhone] = useState("")
+
+    function deleteCustomerByEmail() {
+        setCustomer((customers) =>customers.filter((customers) =>customers.email != email));
+
+    }
     return (
-        <>Delete Customer</>
+        <>
+            {/*<input className={"textInput"} name="firstName" type="text" placeholder="Name" onChange={e =>setName(e.target.value)}/>*/}
+            <input className={"textInput"} name="lastName" type="email" placeholder="Email" onChange={e=>setEmail(e.target.value)}/>
+            {/*<input className={"textInput"} name="lastName" type="tel" placeholder="Phone" onChange={e=>setPhone(e.target.value)}/>*/}
+            <br/>
+            <br/>
+            <br/>
+            <button onClick={deleteCustomerByEmail} type="button">Delete</button>
+            <br/>
+            <br/>
+            {customers.map((customer) =>(
+                <div key={customer.email}>{customer.name+'  '+customer.email+'  '+customer.phone}</div>
+            ))}
+        </>
     );
 }
