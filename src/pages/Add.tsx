344fbec9f0658a733b8596dd1b6ef82c3b163a42
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-12-23 13:52:22 +0530

    create add item section in Add pade

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -13,26 +13,33 @@ export function Add (){
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
 
-    function handleSubmit() {
+    function AddCustomer() {
         const newCustomer = new Customer(name, email, phone);
         setCustomers((customers: Customer[]) => [...customers, newCustomer]);
         navigate('/');
     }
     return (
-        <div className="bg-white shadow-md rounded-lg p-6 m-4">
-            <header>
-                <h2 className="text-2xl font-bold text-gray-700 mb-4">Add Customer</h2>
-            </header>
-            <br />
+        <div className="grid grid-cols-2">
+            {/*write section*/}
+            <div className="bg-white shadow-md rounded-lg p-6 m-4 ">
+                <header>
+                    <h2 className="text-2xl font-bold text-gray-700 mb-4">Add Customer</h2>
+                </header>
+                <br/>
 
-            <CustomerModal
-                handleSubmit={handleSubmit}
-                setName={setName}
-                setEmail={setEmail}
-                setPhone={setPhone}
-            >
-                Add Customer
-            </CustomerModal>
+                <CustomerModal
+                    handleSubmit={AddCustomer}
+                    setName={setName}
+                    setEmail={setEmail}
+                    setPhone={setPhone}
+                >
+                    Add Customer
+                </CustomerModal>
+            </div>
+            {/*left section*/}
+            <div className="bg-white shadow-md rounded-lg p-6 m-4">
+                <h1 className="text-2xl font-bold text-gray-500 mb-6">Add Item</h1>
+            </div>
         </div>
     )
 }
