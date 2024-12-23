c6a822f8df62a31981343c3b2392a082856f720d
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-12-23 15:18:24 +0530

    create view item in dashboard successfully

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,8 @@ import {useContext} from "react";
 import {Customer} from "../models/Customer.ts";
 import {Table} from "../componentes/Table.tsx";
 import {CustomerContext} from "../store/CustomerProvider.tsx";
+import {ItemContext} from "../store/ItemProvider.tsx";
+import {Item} from "../models/Item.ts";
 
 
 /*
@@ -15,7 +17,8 @@ export function Dashboard (){
     );
 }*/
 export function Dashboard() {
-    const [customers,setCustomer] =useContext(CustomerContext)
+    // const [customers,setCustomer] =useContext(CustomerContext)
+    const [items, setItems] = useContext(ItemContext);
     return (
         <div className="p-6 h-lvh bg-amber-100">
             <h1 className="text-3xl font-bold text-gray-700 mb-6">Dashboard</h1>
@@ -32,16 +35,17 @@ export function Dashboard() {
                 <div className="m-3">
                     <h1 className="text-2xl font-bold text-gray-500 mb-6">Item</h1>
                     <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 ">
-                        {customers.map((customer: Customer) => (
+                        {items.map((item: Item) => (
                             <div
-                                id={customer.email}
+                                id={item.name}
                                 className="bg-white shadow-md rounded-lg p-4 border border-gray-200"
                             >
                                 <h2 className="text-lg font-semibold text-blue-600">
-                                    {customer.name}
+                                    {item.name}
                                 </h2>
-                                <p className="text-gray-600">{customer.email}</p>
-                                <p className="text-gray-600">{customer.phone}</p>
+                                <p className="text-gray-600">{item.desc}</p>
+                                <p className="text-gray-600">{item.price}</p>
+                                <p className="text-gray-600">{item.qty}</p>
                             </div>
                         ))}
                     </div>
