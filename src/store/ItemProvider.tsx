1bc58f17370389f87828045a6269e322802fe0ce
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-12-27 12:14:37 +0530

    remake ItemProvider.tsx to reducer

--- src/store/ItemProvider.tsx
+++ src/store/ItemProvider.tsx
@@ -1,14 +1,16 @@
-import {createContext, useState} from "react";
-import {Item} from "../models/Item.ts";
+import {createContext, useReducer, useState} from "react";
+// import {Item} from "../models/Item.ts";
+import {initialState, ItemReducer} from "../reducers/ItemReducer.ts";
 
 export const ItemContext  = createContext();
 export function ItemProvider({children}) {
 
-    const [items, setItems] = useState<Item[]>([]);
+    // const [items, setItems] = useState<Item[]>([]);
+    const [items, dispatch] = useReducer(ItemReducer,initialState);
 
     return (
         <>
-            <ItemContext.Provider value={[items, setItems]}>
+            <ItemContext.Provider value={[items, dispatch]}>
                 {children}
             </ItemContext.Provider>
         </>
