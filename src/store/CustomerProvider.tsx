62e0486b6165846b4300ade5cd62533714368270
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-12-27 10:49:13 +0530

    update CustomerProvider.tsx

--- src/store/CustomerProvider.tsx
+++ src/store/CustomerProvider.tsx
@@ -1,14 +1,16 @@
-import {createContext, useState} from "react";
+import {createContext, useReducer, useState} from "react";
 import {Customer} from "../models/Customer.ts";
+import {CustomerReducer, initialState} from "../reducers/CustomerReducer.ts";
 
 export const CustomerContext  = createContext();
 export function CustomerProvider({children}) {
 
-    const [customers, setCustomers] = useState<Customer[]>([]);
+    // const [customers, setCustomers] = useState<Customer[]>([]);
+    const [customers, dispatch] = useReducer(CustomerReducer, initialState);
 
     return (
         <>
-            <CustomerContext.Provider value={[customers, setCustomers]}>
+            <CustomerContext.Provider value={[customers, dispatch]}>
                 {children}
             </CustomerContext.Provider>
         </>
